d7867d5e347d000718089156d22138803e0c291d
ItsukiKyun <aimanzuhair05@gmail.com>
2025-12-29 14:55:05 +0800

    delete input password lama saat ubah password

--- app/Http/Controllers/User/ProfilController.php
+++ app/Http/Controllers/User/ProfilController.php
@@ -17,19 +17,10 @@ class ProfilController extends Controller
     public function updatePassword(Request $request)
     {
         $request->validate([
-            'current_password' => 'required',
             'password' => 'required|min:8|confirmed',
         ]);
 
         $user = $request->user();
-
-        // cek password lama
-        if (!Hash::check($request->current_password, $user->password)) {
-            return back()->withErrors([
-                'current_password' => 'Password tidak sesuai.',
-            ]);
-        }
-
         $user->password = Hash::make($request->password);
         $user->save();
 
